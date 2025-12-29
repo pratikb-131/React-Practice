2801284be47946d77658106deedd80a50697090d
Pratik Bachhav <patrik.13104@gmail.com>
2025-12-30 01:12:09 +0530

    nested structure using createelement

---
+++ App.js
@@ -0,0 +1,15 @@
+const myNestedElement = React.createElement(
+    'div',
+    { id: 'container', style: { border: '2px solid blue', padding: '20px' } },
+    [
+        React.createElement('h1', { key: 'h1' }, 'I am the Parent'),
+        React.createElement('section', { key: 'sec', style: { color: 'green' } }, [
+            React.createElement('p', { key: 'p1' }, 'I am a nested child paragraph.'),
+            React.createElement('button', { key: 'btn', onClick: () => alert('Hello!') }, 'Click Me')
+        ])
+    ]
+);
+const domContainer = document.getElementById('root');
+const root = ReactDOM.createRoot(domContainer);
+
+root.render(myNestedElement);
